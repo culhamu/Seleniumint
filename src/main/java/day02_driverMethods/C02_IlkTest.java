8576ea3cb719948dde210616c2bd8e8c0ae1250f
Mustafa Culha <merguven753@gmail.com>
2023-02-09 21:06:01 +0300

    2. Gun Sonu

--- src/main/java/day02_driverMethods/C02_IlkTest.java
+++ src/main/java/day02_driverMethods/C02_IlkTest.java
@@ -7,12 +7,33 @@ public class C02_IlkTest {
     public static void main(String[] args) {
         /*
         Gerekli ayarlamaları yaptıktan sonra
-        1- sayfa başlığı amazon kelimesi içermeli
+        1- sayfa başlığı "amazon" kelimesi içermeli
         2- sayfa url'inde "Amazon" kelimesi içermeli
          */
         System.setProperty("Webdriver.chrome.driver","src/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.amazon.com");
-        driver.getTitle();
+        String expectedTitleIcerik="amazon";
+        String expectedUrlIcerik="Amazon";
+
+        String actualUrl=driver.getCurrentUrl();
+        String actualTitle=driver.getTitle();
+
+        //Url test yapalim...
+        if (actualUrl.contains(expectedUrlIcerik)){
+            System.out.println("Url test PASSED");
+        }else {
+            System.out.println("Url test FAILED");
+            System.out.println("actual url : "+actualUrl);
+        }
+        //Title test yapalım...
+        if (actualTitle.contains(expectedTitleIcerik)){
+            System.out.println("Title test PASSED");
+        }else {
+            System.out.println("Title test FAILED");
+            System.out.println("actual title : "+actualTitle);
+        }
+
+        driver.quit();
     }
 }
