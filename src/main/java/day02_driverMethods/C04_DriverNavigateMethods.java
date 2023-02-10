e0ce2629072916bfded497d203548abbb96f2781
Mustafa Culha <merguven753@gmail.com>
2023-02-10 15:06:53 +0300

    2. Gun Sonu

---
+++ src/main/java/day02_driverMethods/C04_DriverNavigateMethods.java
@@ -0,0 +1,20 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C04_DriverNavigateMethods {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.navigate().to("https://www.amazon.com");
+        driver.navigate().to("https://www.youtube.com");
+        Thread.sleep(2000);
+        driver.navigate().back();
+        Thread.sleep(2000);
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        driver.close();
+    }
+}
