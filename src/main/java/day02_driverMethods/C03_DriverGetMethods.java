a3d5fdbbe1ec1f454e3fe6a7849ec71441e56d24
Mustafa Culha <merguven753@gmail.com>
2023-02-10 12:35:40 +0300

    2. Gun Sonu

---
+++ src/main/java/day02_driverMethods/C03_DriverGetMethods.java
@@ -0,0 +1,13 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C03_DriverGetMethods {
+    public static void main(String[] args) {
+        System.setProperty("Webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com");
+
+    }
+}
