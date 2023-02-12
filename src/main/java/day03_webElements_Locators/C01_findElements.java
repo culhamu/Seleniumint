50aa527e1a9e1d705b195fe4387c874fe2791dca
Mustafa Culha <merguven753@gmail.com>
2023-02-12 14:28:56 +0300

    3. Gun Sonu

---
+++ src/main/java/day03_webElements_Locators/C01_findElements.java
@@ -0,0 +1,24 @@
+package day03_webElements_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_findElements {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.navigate().to("https://www.amazon.com");
+
+        WebElement amazonSearchBox= driver.findElement(By.id("twotabsearchtextbox"));
+        amazonSearchBox.sendKeys("Nutella"+ Keys.ENTER);
+        Thread.sleep(1000);
+        driver.close();
+    }
+}
