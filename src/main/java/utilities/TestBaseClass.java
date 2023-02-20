c6b0ac00e2b0f818a59f9708230d84a61a6d7aac
Mustafa Culha <merguven753@gmail.com>
2023-02-20 19:30:55 +0300

    7. Gun Sonu

--- src/main/java/utilities/TestBaseClass.java
+++ src/main/java/utilities/TestBaseClass.java
@@ -1,4 +1,31 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class TestBaseClass {
+    static WebDriver driver;
+    @BeforeClass
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+    @AfterClass
+    public static void teardown(){
+        driver.close();
+    }
+    public void bekle(int saniye){
+        try {
+            Thread.sleep(saniye);
+        } catch (InterruptedException e) {
+
+        }
+    }
 }
