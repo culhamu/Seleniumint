fc1dd409a0efb908aa5c2edf4e728f596d2e1e34
Mustafa Culha <merguven753@gmail.com>
2023-02-24 19:06:44 +0300

    10. Gun Sonu

--- src/main/java/day10_homework/C04_ExplicitlyWait.java
+++ src/main/java/day10_homework/C04_ExplicitlyWait.java
@@ -1,19 +1,39 @@
 package day10_homework;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
+
+import java.time.Duration;
 
 public class C04_ExplicitlyWait extends TestBase {
 
     @Test
     public void test01(){
 
-        //1. Bir class olusturun : EnableTest
-        //2. Bir metod olusturun : isEnabled()
         //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
         //4. Textbox’in etkin olmadigini(enabled) dogrulayın
+        WebElement textBox= driver.findElement(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(textBox.isEnabled());
+
         //5. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
+        WebElement enableButton= driver.findElement(By.xpath("//button[text()='Enable']"));
+        enableButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(textBox));
+
         //6. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+        WebElement actualText= driver.findElement(By.id("message"));
+        Assert.assertTrue(actualText.isDisplayed());
+
+
         //7. Textbox’in etkin oldugunu(enabled) dogrulayın.
+        Assert.assertTrue(textBox.isEnabled());
     }
 }
